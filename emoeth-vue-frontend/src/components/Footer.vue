460ae0f6de541ccaa78b98f2e711dff4cfa7c5b6
Théomé Borck <theome.borck@student.ecp.fr>
2020-04-13 16:48:49 +0200

    Minimize footer

--- emoeth-vue-frontend/src/components/Footer.vue
+++ emoeth-vue-frontend/src/components/Footer.vue
@@ -6,12 +6,13 @@
       width="100%"
       class="secondary text-center"
     >
-      <v-container class="pa-2">
+      <v-container class="pa-1">
         <v-row justify="center" class="white--text" align-it="center">
-          <v-img src="https://cdn.countryflags.com/thumbs/france/flag-round-250.png"
-                 height="16px" max-width="16px" contain style="margin-top: 2px; opacity: 0.9"
+          <v-img
+            src="https://cdn.countryflags.com/thumbs/france/flag-round-250.png"
+            contain class="mt-1 french-flag"
           ></v-img>
-          <div class="ml-3 font-weight-light">
+          <div class="ml-3 font-weight-light caption">
             {{ new Date().getFullYear() }} — <strong> Emoeth Dev.</strong>
           </div>
         </v-row>
@@ -27,5 +28,11 @@
 </script>
 
 <style scoped>
+
+  .french-flag {
+    height: 12px;
+    max-width: 14px;
+    opacity: 0.9;
+  }
 
 </style>
