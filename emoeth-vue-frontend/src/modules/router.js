7a310f615606080b1b6c3be66d116116174aef37
Théomé Borck <theome.borck@student.ecp.fr>
2020-04-28 10:02:33 +0200

    Change landing page

--- emoeth-vue-frontend/src/modules/router.js
+++ emoeth-vue-frontend/src/modules/router.js
@@ -11,17 +11,17 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "home" */ '../views/About.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "home" */ '../views/About.vue'),
+      component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
     },
     {
       path: '/home',
       name: 'home',
       component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "home" */ '../views/About.vue'),
     },
     {
       path: '/blog',
