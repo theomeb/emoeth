f6301ebdfd02c4db91671c74082b1910878a3f8e
Théomé Borck <theome.borck@student.ecp.fr>
2020-06-11 10:24:19 +0200

    Change path on production

--- emoeth-vue-frontend/vue.config.js
+++ emoeth-vue-frontend/vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/emoeth/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 };
