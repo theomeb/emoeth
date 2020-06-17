6d709d05ee94e8e1ab52f0e8039478fb5dca9b4b
Théomé Borck <theome.borck@student.ecp.fr>
2020-06-17 12:16:53 +0200

    Update vue.config.js to handle github pages deployment

--- emoeth-vue-frontend/vue.config.js
+++ emoeth-vue-frontend/vue.config.js
@@ -3,4 +3,5 @@ module.exports = {
     "vuetify"
   ],
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/emoeth/' : '/', if github-pages deployment
 };
